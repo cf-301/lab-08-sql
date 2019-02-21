e78014124bcd301d096c04c76953650f4c1b1aa7
shiratap <spencerhirata@gmail.com>
2019-02-21 11:46:18 -0800

    /locations path works with storing object into db

--- server.js
+++ server.js
@@ -1,103 +1,148 @@
-'use strict'
+'use strict';
 
-//Load environdment Vars from .env
+// Application Dependencies
+const express = require('express');
+const superagent = require('superagent');
+const pg = require('pg');
+const cors = require('cors');
+
+// Load environment variables from .env file
 require('dotenv').config();
 
-// App Dependancies
-const express = require ('express');
-const cors = require('cors');
-const superagent = require('superagent');
-
-//App Setup
-const PORT = process.env.PORT || 3000;
+// Application Setup
 const app = express();
+const PORT = process.env.PORT || 3000;
+
 app.use(cors());
 
-//API routes
+// Database Setup
+const client = new pg.Client(process.env.DATABASE_URL);
+client.connect();
+client.on('error', err => console.error(err));
 
-//locations
+// API Routes
 app.get('/location', (request, response) => {
-  searchToLatLong(request.query.data)
-    .then(location => response.send(location))
+  getLocation(request.query.data)
+    .then(location => {
+      console.log('27', location);
+      response.send(location)
+    })
     .catch(error => handleError(error, response));
-});
-
-//weather
-app.get('/weather', (request, response) => {
-  getWeather(request.query.data)
-    .then(data => response.send(data))
-    .catch(error => handleError(error, response))
 })
 
-//meetups
-app.get('/meetups', (request, response) => {
-  getMeetups(request.query.data)
-    .then(data => response.send(data))
-    .catch(error => handleError(error, response))
-})
+// Do not comment in until you have locations in the DB
+// app.get('/weather', getWeather);
 
-//catch-all error handler
-app.use('*', handleError);
+// Do not comment in until weather is working
+// app.get('/meetups', getMeetups);
 
-// Make sure server is listening for requests
-app.listen(PORT, () => console.log(`App is up on ${PORT}`))
+// Make sure the server is listening for requests
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-
-// Helper Functions
-
-// Err Handler
-function handleError(err, res) {
-  //console.error(err);
-  if (res) res.status(500).send('Sorry, something went terribly wrong. Toodles!');
-}
-
-//creates a new object with our geocode data
-function searchToLatLong(query) {
-  const geoUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GEOCODE_API_KEY}`;
-  return superagent.get(geoUrl)
-    .then(res => new Location(query, res))
-    .catch(error => handleError);
-}
+// *********************
+// MODELS
+// *********************
 
 function Location(query, res) {
   this.search_query = query;
-  this.formatted_query = res.body.results[0].formatted_address;
-  this.latitude = res.body.results[0].geometry.location.lat;
-  this.longitude = res.body.results[0].geometry.location.lng;
+  this.formatted_query = res.formatted_address;
+  this.latitude = res.geometry.location.lat;
+  this.longitude = res.geometry.location.lng;
 }
 
-// creates array of objects with our weather data
-function getWeather(location) {
-  const weatherUrl = `https://api.darksky.net/forecast/${process.env.DARKSKY_API_KEY}/${location.latitude},${location.longitude}`;
-  return superagent.get(weatherUrl)
-    .then(res => {
-      return res.body.daily.data.map(day => new Weather(day));
-    })
-    .catch(error => handleError);
+// function Weather(day) {
+//   this.forecast = day.summary;
+//   this.time = new Date(day.time * 1000).toString().slice(0, 15);
+// }
+
+// function Meetup(meetup) {
+//   this.tableName = 'meetups';
+//   this.link = meetup.link;
+//   this.name = meetup.group.name;
+//   this.creation_date = new Date(meetup.group.created).toString().slice(0, 15);
+//   this.host = meetup.group.who;
+//   this.created_at = Date.now();
+// }
+
+// *********************
+// HELPER FUNCTIONS
+// *********************
+
+function handleError(err, res) {
+  console.error(err);
+  if (res) res.status(500).send('Sorry, something went wrong');
 }
 
-function Weather(day) {
-  this.forecast = day.summary;
-  this.time = new Date(day.time *1000).toString().slice(0,15);
+function getLocation(query) {
+  // CREATE the query string to check for the existence of the location
+  const SQL = `SELECT * FROM locations WHERE search_query=$1;`;
+  const values = [query];
+
+  // Make the query of the database
+  return client.query(SQL, values)
+    .then(result => {
+      // Check to see if the location was found and return the results
+      if (result.rowCount > 0) {
+        console.log('From SQL');
+        return result.rows[0];
+
+        // Otherwise get the location information from the Google API
+      } else {
+        const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GEOCODE_API_KEY}`;
+
+        return superagent.get(url)
+          .then(data => {
+            console.log('FROM API line 90');
+            // Throw an error if there is a problem with the API request
+            if (!data.body.results.length) { throw 'no Data' }
+
+            // Otherwise create an instance of Location
+            else {
+              let location = new Location(query, data.body.results[0]);
+              console.log('98', location);
+
+              // Create a query string to INSERT a new record with the location data
+              let newSQL = `INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ($1, $2, $3, $4) RETURNING id;`;
+              console.log('102', newSQL)
+              let newValues = Object.values(location);
+              console.log('104', newValues)
+
+              // Add the record to the database
+              return client.query(newSQL, newValues)
+                .then(result => {
+                  console.log('108', result.rows);
+                  // Attach the id of the newly created record to the instance of location.
+                  // This will be used to connect the location to the other databases.
+                  console.log('114', result.rows[0].id)
+                  location.id = result.rows[0].id;
+                  return location;
+                })
+                .catch(console.error);
+            }
+          })
+          .catch(error => console.log('Error in SQL Call'));
+      }
+    });
 }
 
-//creates array of objects with our meetup data
-function getMeetups(location) {
-  let meetupsURL = `https://api.meetup.com/find/groups?&query=${location.search_query}&page=20&key=${process.env.MEETUPS_API_KEY}`;
-
-  return superagent.get(meetupsURL)
-    .then(res => {
-      console.log(res.body[0]);
-      return res.body.map(event => new Meetups(event))
-    })
-    .catch(error => handleError)
+// function getWeather(request, response) {
+//   Enter the code from the second printout
+// }
 
 
-}
+// function getMeetups(request, response) {
+//       const url = `https://api.meetup.com/find/upcoming_events?&sign=true&photo-host=public&lon=${request.query.data.longitude}&page=20&lat=${request.query.data.latitude}&key=${process.env.MEETUP_API_KEY}`
 
-function Meetups(event){
-  this.name = event.name;
-  this.link = event.link;
-  this.creation_date = new Date(event.created *1000).toString().slice(0,15);
-  this.host = event.organizer.name;
-}
+//       superagent.get(url)
+//         .then(result => {
+//           const meetups = result.body.events.map(meetup => {
+//             const event = new Meetup(meetup);
+//             return event;
+//           });
+
+//           response.send(meetups);
+//         })
+//         .catch(error => handleError(error, response));
+//     }
+//   })
+
